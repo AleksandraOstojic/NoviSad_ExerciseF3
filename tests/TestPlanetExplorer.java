0f51b869be3d81548861ac83bc90f1216babd321
somename <someemail>
2017-05-12 21:13:54 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -34,5 +34,16 @@ public class TestPlanetExplorer {
 		
 		assertEquals(expected, planet.executeCommand("b"));
 	}
+	@Test
+	public void test_executeCommand4(){
+		
+		PlanetExplorer planet = new PlanetExplorer(100,100, "(0,1)");
+		planet.setPos_x(1);
+		planet.setPos_y(1);
+		planet.setDirection("N");
+		String expected = "(1,1,W)";
+		
+		assertEquals(expected, planet.executeCommand("l"));
+	}
 	
 }
